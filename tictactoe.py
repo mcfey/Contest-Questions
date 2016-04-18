8517c6cb2ded01d644c05fdd1fb9cae319ee1472
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-04-18 14:54:52 -0400

    Updated from Brython Server: 4/18/2016 2:55:39 PM

--- tictactoe.py
+++ tictactoe.py
@@ -4,18 +4,34 @@ from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, El
 black = Color(0x000000, 1.0)
 white = Color(0xffffff, 1.0)
 red = Color(0xff0000, 1.0)
-white = Color(0xffffff, 0.9)
+blue = Color(0x0000ff, 1.0)
 thinline = LineStyle(1, black)
+oline = LineStyle(5, blue)
 
 vline = RectangleAsset(10, 310, thinline, black)
 hline = RectangleAsset(310, 10, thinline, black)
+o = CircleAsset(30, oline, white)
 
 Sprite(vline, (140, 40))
 Sprite(vline, (240, 40))
 Sprite(hline, (40, 140))
 Sprite(hline, (40, 240))
 
+Sprite(o
+
 #choice = input("Would you like to be X's or O's? )
+
+
+
+
+class TTT(app):
+    
+    def __init__(self):
+        self.listenMouseEvent( 'click', self.click)
+
+    def click(self, event):
+        x = event.x
+        y = event.y
 
 myapp = App()
 myapp.run()
