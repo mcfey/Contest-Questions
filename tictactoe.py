ac690754e5055f6622d8c0642b4d7506a3fb9029
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-04-18 14:42:05 -0400

    Updated from Brython Server: 4/18/2016 2:42:52 PM

--- tictactoe.py
+++ tictactoe.py
@@ -1,2 +1,15 @@
 
-choice = input("Would you like to be X's or O's? )
+red = Color(0xff0000, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+white = Color(0xffffff, 0.9)
+thinline = LineStyle(1, black)
+
+verticleline = RectangleAsset(20, 300, thinline, black)
+horizontalline = RectangleAsset(300, 20, thinline, black)
+Sprite(verticleline, (110, 10))
+Sprite(verticleline, (210, 10))
+Sprite(horizontalline, (10, 110))
+Sprite(horizontalline, (10, 210))
+
+#choice = input("Would you like to be X's or O's? )
