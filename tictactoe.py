89f15abc86a255df740f4583d99591746a3d5114
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-04-19 14:12:06 -0400

    Updated from Brython Server: 4/19/2016 2:12:06 PM

--- tictactoe.py
+++ tictactoe.py
@@ -25,7 +25,7 @@ Sprite(o
 #choice = input("Would you like to be X's or O's? )
 
 
-class TTT(App):
+class Ttt(App):
     
     def __init__(self):
         self.listenMouseEvent( 'click', self.click)
