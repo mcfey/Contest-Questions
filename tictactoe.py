d3ab9389a579a638d379aa5d749a9b2b3bf0399f
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-04-26 14:36:48 -0400

    Updated from Brython Server: 4/26/2016 2:36:47 PM

--- tictactoe.py
+++ tictactoe.py
@@ -22,7 +22,9 @@ oshape = CircleAsset(30, oline, clear)
 xshape = PolygonAsset([(5,5),(35,35),(65,5),(5,65),(35,35),(65,65)], xline, red)
 
 w = PolygonAsset([(5,5),(15,30),(25,15),(35,30),(45,5),(35,30),(25,15),(15,30)], thickline, clear)
+i = PolygonAsset([(5,5),(5,30)], thickline, clear)
 Sprite(w, (400, 160))
+Sprite(w, (430, 160))
 
 Sprite(vline, (140, 40))
 Sprite(vline, (240, 40))
