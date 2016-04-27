2c05994a006d941f7ceee114fb28c0a50abf929d
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-04-27 13:44:51 -0400

    Updated from Brython Server: 4/27/2016 1:44:53 PM

--- tictactoe.py
+++ tictactoe.py
@@ -78,21 +78,6 @@ class Ttt(App):
     def __init__(self):
         super().__init__() 
         self.listenMouseEvent( 'click', self.click)
-    
-    def click(self, event):
-        
-        for s in playeralive:
-            for sp in playeralive:
-                for spr in playeralive:
-                    if (s.x == sp.x == spr.x) or (s.y == sp.y == spr.y) or (s.y+100==sp.y and sp.y+100==spr.y and s.x+100==sp.x and sp.x+100==spr.x):
-                        Sprite(playershape, (460, 110))
-                        Sprite(w, (400, 160))
-                        Sprite(i, (455, 165))
-                        Sprite(n, (470, 165))
-                        Sprite(s, (514, 174))
-                        Sprite(s, (514, 187))
-                        Sprite(scover, (522,174))
-                        Sprite(scover, (505,185))
 
     
     def click(self, event):
@@ -117,7 +102,20 @@ class Ttt(App):
                     if abs(spr.x - xorandom.x) <= 40 and abs(xorandom.y - spr.y) <= 40:
                         player.remove(spr)
 
-
+    def step(self):
+            
+            for s in playeralive:
+                for sp in playeralive:
+                    for spr in playeralive:
+                        if (s.x == sp.x == spr.x) or (s.y == sp.y == spr.y) or (s.y+100==sp.y and sp.y+100==spr.y and s.x+100==sp.x and sp.x+100==spr.x):
+                            Sprite(playershape, (460, 110))
+                            Sprite(w, (400, 160))
+                            Sprite(i, (455, 165))
+                            Sprite(n, (470, 165))
+                            Sprite(s, (514, 174))
+                            Sprite(s, (514, 187))
+                            Sprite(scover, (522,174))
+                            Sprite(scover, (505,185))
 
 myapp = Ttt()
 myapp.run()
